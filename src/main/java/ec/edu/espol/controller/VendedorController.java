193934b9f689b1b78207638e43a508984f43c33a
Ronny García <86664145+rsgarcia0203@users.noreply.github.com>
2021-09-06 00:09:44 -0500

    Update VendedorController.java

--- src/main/java/ec/edu/espol/controller/VendedorController.java
+++ src/main/java/ec/edu/espol/controller/VendedorController.java
@@ -7,39 +7,124 @@ package ec.edu.espol.controller;
 
 import ec.edu.espol.model.Comprador;
 import ec.edu.espol.model.CompradorException;
+import ec.edu.espol.model.Oferta;
+import ec.edu.espol.model.Vehiculo;
 import ec.edu.espol.model.Vendedor;
 import ec.edu.espol.model.VendedorException;
 import ec.edu.espol.proyecto2p.App;
 import ec.edu.espol.util.Util;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
 /**
+ * FXML Controller class
  *
- * @author User
+ * @author rsgar
  */
-public class VendedorController {
+public class VendedorController implements Initializable {
 
     @FXML
-    private TextField txtCorreo;
+    private Label lbl_nombre;
     @FXML
-    private TextField txtNombres;
+    private TableView<Vehiculo> tableVehiculos;
     @FXML
-    private TextField txtApellidos;
+    private TableColumn<Vehiculo, Integer> id_column;
     @FXML
-    private TextField txtOrganizacion;
+    private TableColumn<Vehiculo, String> tipos_column;
+    @FXML
+    private TableColumn<Vehiculo, String> placas_column;
+    @FXML
+    private TableColumn<Vehiculo, String> marca_column;
+    @FXML
+    private TableColumn<Vehiculo, String> modelo_column;
+    @FXML
+    private TableColumn<Vehiculo, String> motor_column;
+    @FXML
+    private TableColumn<Vehiculo, Integer> anio_column;
+    @FXML
+    private TableColumn<Vehiculo, Double> recorrido_column;
+    @FXML
+    private TableColumn<Vehiculo, String> color_column;
+    @FXML
+    private TableColumn<Vehiculo, String> combustible_column;
+    @FXML
+    private TableColumn<Vehiculo, String> vidrios_column;
+    @FXML
+    private TableColumn<Vehiculo, String> transmision_column;
+    @FXML
+    private TableColumn<Vehiculo, String> traccion_column;
+    @FXML
+    private TableColumn<Vehiculo, Double> precio_column;
+    @FXML
+    private ComboBox cbx_tipos;
+    @FXML
+    private GridPane contenedor1;
+    @FXML
+    private TextField txt_placa;
+    @FXML
+    private TextField txt_marca;
+    @FXML
+    private TextField txt_modelo;
+    @FXML
+    private TextField txt_motor;
+    @FXML
+    private TextField txt_anio;
+    @FXML
+    private TextField txt_recorrido;
+    @FXML
+    private TextField txt_color;
+    @FXML
+    private TextField txt_combustible;
+    @FXML
+    private TextField txt_precio;
+    @FXML
+    private GridPane contenedor2;
+    @FXML
+    private TextField txt_transmision;
+    @FXML
+    private TextField txt_vidrios;
+    @FXML
+    private GridPane contenedor3;
+    @FXML
+    private TextField txt_traccion;
+    @FXML
+    private Button btn_registrar;
+    @FXML
+    private TextField placas;
+    @FXML
+    private TextField txt_correo;
+    @FXML
+    private TextField txt_nombres;
+    @FXML
+    private TextField txt_apellidos;
+    @FXML
+    private TextField txt_organizacion;
     @FXML
     private VBox rol_contenedor;
     @FXML
@@ -59,27 +144,92 @@ public class VendedorController {
     @FXML
     private PasswordField txt_clave_confirmacion;
     @FXML
-    private RadioButton btnAuto;
+    private TableView<Oferta> tableOfertas;
     @FXML
-    private RadioButton btnCamioneta;
+    private TableColumn<Oferta, Integer> idO_column;
     @FXML
-    private RadioButton btnMoto;
-    
-    private ArrayList<Vendedor> vendedores;
-    
-    private ArrayList<Comprador> compradores;
-    
+    private TableColumn<Oferta, String> tiposO_column;
+    @FXML
+    private TableColumn<Oferta, String> placasO_column;
+    @FXML
+    private TableColumn<Oferta, Double> precioV_column;
+    @FXML
+    private TableColumn<Oferta, Double> precioO_column;
+    private ArrayList<Vehiculo> vehiculosFile;
     private Vendedor vendedor;
-    
+    private ArrayList<Vehiculo> vehicles;
+    private ArrayList<Comprador> compradores;
+    private ArrayList<Vendedor> vendedores;
+    private Vehiculo vehiculoSel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            this.vehiculosFile = Vehiculo.readFile("src\\main\\resources\\doc\\vehiculo.txt");
+            ArrayList<String> tipos = new ArrayList<>();
+            tipos.add("Automovil");
+            tipos.add("Motocicleta");
+            tipos.add("Camioneta");
+            cbx_tipos.setItems(FXCollections.observableArrayList(tipos));
+            if (!(this.vendedor.getVehiculos().isEmpty())) {
+                this.vehicles = this.vendedor.getVehiculos();
+                ObservableList<Vehiculo> vehiculos = FXCollections.observableArrayList(this.vehicles);
+                id_column.setCellValueFactory(new PropertyValueFactory<>("ID"));
+                tipos_column.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+                placas_column.setCellValueFactory(new PropertyValueFactory<>("placa"));
+                marca_column.setCellValueFactory(new PropertyValueFactory<>("marca"));
+                modelo_column.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+                motor_column.setCellValueFactory(new PropertyValueFactory<>("tipo_motor"));
+                anio_column.setCellValueFactory(new PropertyValueFactory<>("año"));
+                recorrido_column.setCellValueFactory(new PropertyValueFactory<>("recorrido"));
+                color_column.setCellValueFactory(new PropertyValueFactory<>("color"));
+                combustible_column.setCellValueFactory(new PropertyValueFactory<>("tipo_combustible"));
+                vidrios_column.setCellValueFactory(new PropertyValueFactory<>("vidrios"));
+                transmision_column.setCellValueFactory(new PropertyValueFactory<>("transmision"));
+                traccion_column.setCellValueFactory(new PropertyValueFactory<>("traccion"));
+                precio_column.setCellValueFactory(new PropertyValueFactory<>("precio"));
+                tableVehiculos.setItems(vehiculos);
+                tableVehiculos.setOnMouseClicked((MouseEvent e) -> {
+                    Vehiculo vehiculoSelec = tableVehiculos.getSelectionModel().getSelectedItem();
+                    if (vehiculoSelec != null) {
+                        setVehiculo(vehiculoSelec);
+                    } else {
+                        Alert a = new Alert(Alert.AlertType.WARNING, "Debe seleccionar un vehículo.");
+                        a.show();
+                    }
+                });
+            }
+
+        } catch (FileNotFoundException ex) {
+            Alert a = new Alert(Alert.AlertType.WARNING, "Documento no encontrado.");
+            a.show();
+        } catch (NumberFormatException ex) {
+            Alert a = new Alert(Alert.AlertType.WARNING, "Debe de ingresar un precio para ofertar.");
+            a.show();
+        }
+    }
+
     public void setVendedor(Vendedor vendedor) {
         this.vendedor = vendedor;
-        txtNombres.setText(this.vendedor.getNombres());
-        txtApellidos.setText(this.vendedor.getApellidos());
-        txtCorreo.setText(this.vendedor.getCorreo());
-        txtOrganizacion.setText(this.vendedor.getOrganizacion());
-
+        txt_nombres.setText(this.vendedor.getNombres());
+        txt_apellidos.setText(this.vendedor.getApellidos());
+        txt_correo.setText(this.vendedor.getCorreo());
+        txt_organizacion.setText(this.vendedor.getOrganizacion());
+        lbl_nombre.setText(this.vendedor.getNombres() + " " + this.vendedor.getApellidos());
     }
-    
+
+    public void setCompradores(ArrayList<Comprador> compradores) {
+        this.compradores = compradores;
+    }
+
+    public void setVendedores(ArrayList<Vendedor> vendedores) {
+        this.vendedores = vendedores;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculoSel = vehiculo;
+    }
+
     public void toMain() {
         try {
             FXMLLoader fxmlloader = App.loadFXMLoader("Login");
@@ -88,6 +238,149 @@ public class VendedorController {
             Alert a3 = new Alert(Alert.AlertType.ERROR, "No se pudo abrir el archivo del siguiente grafo de scene");
             a3.show();
         }
+    }
+
+    @FXML
+    private void cambiar(ActionEvent event) {
+        String item = (String) cbx_tipos.getValue();
+        if (item != null) {
+            btn_registrar.setOpacity(1);
+            btn_registrar.setDisable(false);
+            if (item.equals("Motocicleta")) {
+                activarCont1();
+                desactivarCont2();
+                desactivarCont3();
+            } else if (item.equals("Automovil")) {
+                activarCont1();
+                activarCont2();
+                desactivarCont3();
+            } else if (item.equals("Camioneta")) {
+                activarCont1();
+                activarCont2();
+                activarCont3();
+            }
+        }
+    }
+
+    @FXML
+    private void Registrar(MouseEvent event) {
+        try {
+            String item = (String) cbx_tipos.getValue();
+            if (item != null) {
+                int id = vehiculosFile.size() + 1;
+                if (item.equals("Motocicleta")) {
+                    String placa = txt_placa.getText();
+                    String marca = txt_marca.getText();
+                    String modelo = txt_modelo.getText();
+                    String motor = txt_motor.getText();
+                    int año = Integer.parseInt(txt_anio.getText());
+                    double recorrido = Double.parseDouble(txt_recorrido.getText());
+                    String color = txt_color.getText();
+                    String combustible = txt_combustible.getText();
+                    double precio = Double.parseDouble(txt_precio.getText());
+                    if (placa.equals("") || marca.equals("") || modelo.equals("") || motor.equals("") || (txt_anio.getText()).equals("") || (txt_recorrido.getText()).equals("") || color.equals("") || combustible.equals("") || (txt_precio.getText()).equals("")) {
+                        Alert a = new Alert(Alert.AlertType.WARNING, "Debe llenar todos los campos.");
+                        a.show();
+                    } else {
+                        Vehiculo v = new Vehiculo(id, this.vendedor.getID(), placa, marca, modelo, motor, año, recorrido, color, combustible, precio);
+                        registrarVehiculo(v);
+                    }
+                } else if (item.equals("Automovil")) {
+                    String placa = txt_placa.getText();
+                    String marca = txt_marca.getText();
+                    String modelo = txt_modelo.getText();
+                    String motor = txt_motor.getText();
+                    int año = Integer.parseInt(txt_anio.getText());
+                    double recorrido = Double.parseDouble(txt_recorrido.getText());
+                    String color = txt_color.getText();
+                    String combustible = txt_combustible.getText();
+                    double precio = Double.parseDouble(txt_precio.getText());
+                    String transmision = txt_transmision.getText();
+                    String vidrios = txt_vidrios.getText();
+                    if (placa.equals("") || marca.equals("") || modelo.equals("") || motor.equals("") || (txt_anio.getText()).equals("") || (txt_recorrido.getText()).equals("") || color.equals("") || combustible.equals("") || (txt_precio.getText()).equals("") || transmision.equals("") || vidrios.equals("")) {
+                        Alert a = new Alert(Alert.AlertType.WARNING, "Debe llenar todos los campos.");
+                        a.show();
+                    } else {
+                        Vehiculo v = new Vehiculo(id, this.vendedor.getID(), placa, marca, modelo, motor, año, recorrido, color, combustible, precio, transmision, vidrios);
+                        registrarVehiculo(v);
+                    }
+                } else if (item.equals("Camioneta")) {
+                    String placa = txt_placa.getText();
+                    String marca = txt_marca.getText();
+                    String modelo = txt_modelo.getText();
+                    String motor = txt_motor.getText();
+                    int año = Integer.parseInt(txt_anio.getText());
+                    double recorrido = Double.parseDouble(txt_recorrido.getText());
+                    String color = txt_color.getText();
+                    String combustible = txt_combustible.getText();
+                    double precio = Double.parseDouble(txt_precio.getText());
+                    String transmision = txt_transmision.getText();
+                    String vidrios = txt_vidrios.getText();
+                    String traccion = txt_traccion.getText();
+                    if (placa.equals("") || marca.equals("") || modelo.equals("") || motor.equals("") || (txt_anio.getText()).equals("") || (txt_recorrido.getText()).equals("") || color.equals("") || combustible.equals("") || (txt_precio.getText()).equals("") || transmision.equals("") || vidrios.equals("") || traccion.equals("")) {
+                        Alert a = new Alert(Alert.AlertType.WARNING, "Debe llenar todos los campos.");
+                        a.show();
+                    } else {
+                        Vehiculo v = new Vehiculo(id, this.vendedor.getID(), placa, marca, modelo, motor, año, recorrido, color, combustible, precio, transmision, vidrios, traccion);
+                        registrarVehiculo(v);
+                    }
+                }
+            }
+        } catch (NumberFormatException ex) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Solo se puede ingresar números.");
+            a.showAndWait();
+        }
+
+    }
+
+    public void registrarVehiculo(Vehiculo v) {
+        Alert a = new Alert(Alert.AlertType.CONFIRMATION, "¿Está seguro de registrar el vehículo con esos datos?.");
+        a.setTitle("SYSTEM-POO");
+        a.setHeaderText("Confirmación de datos de registro");
+        Optional<ButtonType> resultado = a.showAndWait();
+        if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
+            v.saveFile("src\\main\\resources\\doc\\vehiculo.txt");
+            Alert a2 = new Alert(Alert.AlertType.INFORMATION, "Vehiculo registrado con éxito.");
+            a2.showAndWait();
+            toMain();
+        }
+    }
+
+    public void activarCont1() {
+        contenedor1.setOpacity(1);
+        contenedor1.setDisable(false);
+    }
+
+    public void activarCont2() {
+        contenedor2.setOpacity(1);
+        contenedor2.setDisable(false);
+    }
+
+    public void desactivarCont2() {
+        contenedor2.setOpacity(0);
+        contenedor2.setDisable(true);
+    }
+
+    public void activarCont3() {
+        contenedor3.setOpacity(1);
+        contenedor3.setDisable(false);
+    }
+
+    public void desactivarCont3() {
+        contenedor3.setOpacity(0);
+        contenedor3.setDisable(true);
+    }
+
+    @FXML
+    private void buscar(MouseEvent event) {
+    }
+
+    @FXML
+    private void aceptarOferta(MouseEvent event) {
+    }
+
+    @FXML
+    private void declinarOferta(MouseEvent event) {
     }
 
     @FXML
@@ -104,7 +397,6 @@ public class VendedorController {
         rol_contenedor.setDisable(false);
         contra_contenedor.setOpacity(0);
         contra_contenedor.setDisable(true);
-        
     }
 
     @FXML
@@ -119,20 +411,19 @@ public class VendedorController {
                 a.setHeaderText("Confirmación de datos de registro");
                 Optional<ButtonType> resultado = a.showAndWait();
                 if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
-                    Comprador.registrarNuevoComprador(this.vendedor.getNombres(), this.vendedor.getApellidos(), this.vendedor.getOrganizacion(), this.vendedor.getCorreo(), this.vendedor.getClave(),this.compradores, "src\\main\\resources\\doc\\vendedor.txt");
+                    Comprador.registrarNuevoComprador(this.vendedor.getNombres(), this.vendedor.getApellidos(), this.vendedor.getOrganizacion(), this.vendedor.getCorreo(), this.vendedor.getClave(), this.compradores, "src\\main\\resources\\doc\\vendedor.txt");
                     Vendedor.eliminarVendedor(vendedores, vendedor);
                     Alert a2 = new Alert(Alert.AlertType.INFORMATION, "Rol cambiado con éxito.");
                     a2.showAndWait();
                     toMain();
-                }              
-
+                }
             } else if (tipo.getSelectedToggle() == rb_ambos) {
                 Alert a = new Alert(Alert.AlertType.CONFIRMATION, "¿Está seguro de cambiar de rol?.");
                 a.setTitle("SYSTEM-POO");
                 a.setHeaderText("Confirmación de datos de registro");
                 Optional<ButtonType> resultado = a.showAndWait();
                 if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
-                    Comprador.registrarNuevoComprador(this.vendedor.getNombres(), this.vendedor.getApellidos(), this.vendedor.getOrganizacion(), this.vendedor.getCorreo(), this.vendedor.getClave(),this.compradores, "src\\main\\resources\\doc\\vendedor.txt");
+                    Comprador.registrarNuevoComprador(this.vendedor.getNombres(), this.vendedor.getApellidos(), this.vendedor.getOrganizacion(), this.vendedor.getCorreo(), this.vendedor.getClave(), this.compradores, "src\\main\\resources\\doc\\vendedor.txt");
                     Alert a2 = new Alert(Alert.AlertType.INFORMATION, "Rol cambiado con éxito.");
                     a2.showAndWait();
                     toMain();
@@ -149,7 +440,6 @@ public class VendedorController {
             a.show();
         }
     }
- 
 
     @FXML
     private void cambiarContra(MouseEvent event) {
@@ -182,10 +472,9 @@ public class VendedorController {
                     a.show();
                 }
             } catch (NoSuchAlgorithmException ex) {
-                ex.printStackTrace();
+
             }
         }
     }
 
-    
 }
