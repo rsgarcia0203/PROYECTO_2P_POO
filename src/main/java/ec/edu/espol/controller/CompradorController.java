ff6d2932735b6f78717efa88034a5929f5ac9364
Ronny García <86664145+rsgarcia0203@users.noreply.github.com>
2021-09-06 00:08:38 -0500

    Update CompradorController.java

--- src/main/java/ec/edu/espol/controller/CompradorController.java
+++ src/main/java/ec/edu/espol/controller/CompradorController.java
@@ -5,18 +5,16 @@
  */
 package ec.edu.espol.controller;
 
-import ec.edu.espol.model.Automovil;
-import ec.edu.espol.model.Camioneta;
 import ec.edu.espol.model.Comprador;
 import ec.edu.espol.model.CompradorException;
-import ec.edu.espol.model.Ingreso;
-import ec.edu.espol.model.Motocicleta;
+import ec.edu.espol.model.Venta;
 import ec.edu.espol.model.Oferta;
 import ec.edu.espol.model.Vehiculo;
 import ec.edu.espol.model.Vendedor;
 import ec.edu.espol.model.VendedorException;
 import ec.edu.espol.proyecto2p.App;
 import ec.edu.espol.util.Util;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
@@ -29,7 +27,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
@@ -83,6 +84,8 @@ public class CompradorController implements Initializable {
     @FXML
     private TableColumn<Vehiculo, Integer> id_column;
     @FXML
+    private TableColumn<Vehiculo, String> tipos_column;
+    @FXML
     private TableColumn<Vehiculo, String> placas_column;
     @FXML
     private TableColumn<Vehiculo, String> marca_column;
@@ -106,6 +109,25 @@ public class CompradorController implements Initializable {
     private TableColumn<Vehiculo, String> traccion_column;
     @FXML
     private TableColumn<Vehiculo, Double> precio_column;
+    @FXML
+    private Label lbl_nombre;
+    @FXML
+    private ComboBox cbx_tipos;
+    @FXML
+    private TextField recorridoIni;
+    @FXML
+    private TextField recorridoFin;
+    @FXML
+    private TextField anioIni;
+    @FXML
+    private TextField anioFin;
+    @FXML
+    private TextField precioIni;
+    @FXML
+    private TextField precioFin;
+    private ArrayList<Vehiculo> vehicles;
+    @FXML
+    private TextField precioOfer;
 
     /**
      * Initializes the controller class.
@@ -115,8 +137,64 @@ public class CompradorController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //Eliminar essto para poder cargar la ventana
-        
+        try {
+            ArrayList<String> tipos = new ArrayList<>();
+            tipos.add("Todos");
+            tipos.add("Automovil");
+            tipos.add("Motocicleta");
+            tipos.add("Camioneta");
+            cbx_tipos.setItems(FXCollections.observableArrayList(tipos));
+            this.vehicles = Vehiculo.readFile("src\\main\\resources\\doc\\vehiculo.txt");
+            ObservableList<Vehiculo> vehiculos = FXCollections.observableArrayList(this.vehicles);
+            id_column.setCellValueFactory(new PropertyValueFactory<>("ID"));
+            tipos_column.setCellValueFactory(new PropertyValueFactory<>("tipo"));
+            placas_column.setCellValueFactory(new PropertyValueFactory<>("placa"));
+            marca_column.setCellValueFactory(new PropertyValueFactory<>("marca"));
+            modelo_column.setCellValueFactory(new PropertyValueFactory<>("modelo"));
+            motor_column.setCellValueFactory(new PropertyValueFactory<>("tipo_motor"));
+            anio_column.setCellValueFactory(new PropertyValueFactory<>("año"));
+            recorrido_column.setCellValueFactory(new PropertyValueFactory<>("recorrido"));
+            color_column.setCellValueFactory(new PropertyValueFactory<>("color"));
+            combustible_column.setCellValueFactory(new PropertyValueFactory<>("tipo_combustible"));
+            vidrios_column.setCellValueFactory(new PropertyValueFactory<>("vidrios"));
+            transmision_column.setCellValueFactory(new PropertyValueFactory<>("transmision"));
+            traccion_column.setCellValueFactory(new PropertyValueFactory<>("traccion"));
+            precio_column.setCellValueFactory(new PropertyValueFactory<>("precio"));
+            tableOfertas.setItems(vehiculos);
+            tableOfertas.setOnMouseClicked((MouseEvent e) -> {
+                Vehiculo vehiculoSel = tableOfertas.getSelectionModel().getSelectedItem();
+                if (vehiculoSel != null) {
+                    if (!precioOfer.getText().equals("")) {
+                        double precioOfertado = Double.parseDouble(precioOfer.getText());
+                        Alert a = new Alert(Alert.AlertType.CONFIRMATION, "¿Desea ofertar $" + precioOfertado + " por este vehículo?.");
+                        a.setTitle("SYSTEM-POO");
+                        a.setHeaderText("Confirmación de oferta");
+                        Optional<ButtonType> resultado = a.showAndWait();
+                        if (resultado.isPresent() && resultado.get() == ButtonType.OK) {
+                            Alert a2 = new Alert(Alert.AlertType.INFORMATION, "Rol cambiado con éxito.");
+                            a2.showAndWait();
+                            ArrayList<Vehiculo> Nvehiculos = (ArrayList<Vehiculo>) tableOfertas.getItems();
+                            Nvehiculos.remove(vehiculoSel);
+                            Oferta.registrarNuevaOferta(vehiculoSel, comprador, precioOfertado, "oferta.txt");
+                            tableOfertas.setItems(FXCollections.observableArrayList(Nvehiculos));
+                        }
+                    } else {
+                        Alert a = new Alert(Alert.AlertType.WARNING, "Para ofertar debe ingresar un precio a ofertar por el vehículo seleccionado.");
+                        a.show();
+                    }
+                } else {
+                    Alert a = new Alert(Alert.AlertType.WARNING, "Debe seleccionar un vehículo.");
+                    a.show();
+                }
+            });
+        } catch (FileNotFoundException ex) {
+            Alert a = new Alert(Alert.AlertType.WARNING, "Documento no encontrado.");
+            a.show();
+        } catch (NumberFormatException ex) {
+            Alert a = new Alert(Alert.AlertType.WARNING, "Debe de ingresar un precio para ofertar.");
+            a.show();
+        }
+
     }
 
     public void setComprador(Comprador comprador) {
@@ -125,7 +203,7 @@ public class CompradorController implements Initializable {
         txt_apellidos.setText(this.comprador.getApellidos());
         txt_correo.setText(this.comprador.getCorreo());
         txt_organizacion.setText(this.comprador.getOrganizacion());
-
+        lbl_nombre.setText(this.comprador.getNombres() + " " + this.comprador.getApellidos());
     }
 
     public void setCompradores(ArrayList<Comprador> compradores) {
@@ -160,7 +238,6 @@ public class CompradorController implements Initializable {
         rol_contenedor.setDisable(false);
         contra_contenedor.setOpacity(0);
         contra_contenedor.setDisable(true);
-
     }
 
     @FXML
@@ -181,7 +258,6 @@ public class CompradorController implements Initializable {
                     a2.showAndWait();
                     toMain();
                 }
-
             } else if (tipo.getSelectedToggle() == rb_ambos) {
                 Alert a = new Alert(Alert.AlertType.CONFIRMATION, "¿Está seguro de cambiar de rol?.");
                 a.setTitle("SYSTEM-POO");
@@ -241,4 +317,97 @@ public class CompradorController implements Initializable {
             }
         }
     }
+    
+    @FXML
+    private void buscar(MouseEvent event) {
+        try {
+            double recorridoInicial = Double.parseDouble(recorridoIni.getPromptText());
+            double recorridoFinal = Double.parseDouble(recorridoFin.getPromptText());
+            if (!recorridoIni.getText().equals("") && !recorridoFin.getText().equals("")) {
+                recorridoInicial = Double.parseDouble(recorridoIni.getText());
+                recorridoFinal = Double.parseDouble(recorridoFin.getText());
+            } else if((!recorridoIni.getText().equals("") && recorridoFin.getText().equals("")) || (recorridoIni.getText().equals("") && !recorridoFin.getText().equals(""))){
+                Alert a = new Alert(Alert.AlertType.WARNING, "Debe de llenar el inicio y fin del parámetro de busqueda.");
+                a.setTitle("SYSTEM-POO");
+                a.setHeaderText("Rangos de recorridos.");
+                a.showAndWait();
+            } else {
+                recorridoInicial = Double.parseDouble(recorridoIni.getPromptText());
+                recorridoFinal = Double.parseDouble(recorridoFin.getPromptText());
+            }
+
+            int añoInicial = Integer.parseInt(anioIni.getPromptText());
+            int añoFinal = Integer.parseInt(anioFin.getPromptText());
+            if (!anioIni.getText().equals("") && !anioFin.getText().equals("")) {
+                añoInicial = Integer.parseInt(anioIni.getText());
+                añoFinal = Integer.parseInt(anioFin.getText());
+            } else if((!anioIni.getText().equals("") && anioFin.getText().equals("")) || (anioIni.getText().equals("") && !anioFin.getText().equals(""))){
+                Alert a = new Alert(Alert.AlertType.WARNING, "Debe de llenar el inicio y fin del parámetro de busqueda.");
+                a.setTitle("SYSTEM-POO");
+                a.setHeaderText("Rangos de años.");
+                a.showAndWait();
+            } else {
+                añoInicial = Integer.parseInt(anioIni.getPromptText());
+                añoFinal = Integer.parseInt(anioFin.getPromptText());
+            }
+
+            double precioInicial = Double.parseDouble(precioIni.getPromptText());
+            double precioFinal = Double.parseDouble(precioFin.getPromptText());
+            if (!precioIni.getText().equals("") && !precioFin.getText().equals("")) {
+                precioInicial = Double.parseDouble(precioIni.getText());
+                precioFinal = Double.parseDouble(precioFin.getText());
+            } else if((!precioIni.getText().equals("") && precioFin.getText().equals("")) || (precioIni.getText().equals("") && !precioFin.getText().equals("")) ){
+                Alert a = new Alert(Alert.AlertType.WARNING, "Debe de llenar el inicio y fin del parámetro de busqueda.");
+                a.setTitle("SYSTEM-POO");
+                a.setHeaderText("Rangos de precios.");
+                a.showAndWait();
+            } else {
+                precioInicial = Double.parseDouble(precioIni.getPromptText());
+                precioFinal = Double.parseDouble(precioFin.getPromptText());
+            }
+            
+            String item = (String) cbx_tipos.getValue();
+            if (item != null) {
+                ArrayList<Vehiculo> vehiculos = this.vehicles;
+
+                if (!item.equals("Todos")) {
+                    vehiculos = Vehiculo.vehiculosxtipo(vehiculos, item);
+                    tableOfertas.setItems(FXCollections.observableArrayList(Vehiculo.vehiculosxtipo(vehiculos, item)));
+                } else {
+                    vehiculos = this.vehicles;
+                    tableOfertas.setItems(FXCollections.observableArrayList(vehiculos));
+                }
+
+                if (recorridoInicial != 0 && recorridoFinal != 0) {
+                    vehiculos = Vehiculo.vehiculosxRecorrido(vehiculos, recorridoInicial, recorridoFinal);
+                    tableOfertas.setItems(FXCollections.observableArrayList(Vehiculo.vehiculosxRecorrido(vehiculos, recorridoInicial, recorridoFinal)));
+                }
+
+                if (añoInicial != 0 && añoFinal != 0) {
+                    vehiculos = Vehiculo.vehiculosxAño(vehiculos, añoInicial, añoFinal);
+                    tableOfertas.setItems(FXCollections.observableArrayList(Vehiculo.vehiculosxAño(vehiculos, añoInicial, añoFinal)));
+                }
+
+                if (precioInicial != 0 && precioFinal != 0) {
+                    vehiculos = Vehiculo.vehiculosxPrecio(vehiculos, precioInicial, precioFinal);
+                    tableOfertas.setItems(FXCollections.observableArrayList(Vehiculo.vehiculosxPrecio(vehiculos, precioInicial, precioFinal)));
+                }
+                double precioOfertado;
+                if (!vehiculos.isEmpty()) {
+
+                } else {
+                    Alert a = new Alert(Alert.AlertType.INFORMATION, "No existen vehículos con esos parametros de búsqueda.");
+                    a.showAndWait();
+                }
+            } else {
+                Alert a = new Alert(Alert.AlertType.WARNING, "Debe elegir un tipo.");
+                a.showAndWait();
+            }
+        } catch (NumberFormatException e) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Solo se puede ingresar números.");
+            a.showAndWait();
+        }
+
+    }
+
 }
